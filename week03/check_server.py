799940f100ac4dd8bfc51e1f46c7ee7660f4987b
Allen <allentango@163.com>
2020-07-13 23:08:11 +0800

    update

--- week03/check_server.py
+++ week03/check_server.py
@@ -11,6 +11,7 @@ pmap.py -n 10 -f tcp -ip 192.168.0.1 -w result.json
 
 # print("-" in sys.argv[6])
 
+
 def iplist():
     if "-" in sys.argv[6]:
         addrs = sys.argv[6].split("-")
@@ -31,35 +32,44 @@ def iplist():
 # PORT = 5555
 _lock = threading.Lock()
 
+
 def check_server(address, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(0.5)
-    print(f"Attempting to connect to {address} on {port} ")
+    print(f"Attempting to connect to {address} on {port}")
     try:
         s.connect((address, port))
         with _lock:
-            print(f"Connected to {address} on {port}.")
+            return f"Connected to {address} on {port}."
     except socket.error as err:
-        print(f"Connetion to {address} on {port} failed: {err} ")
+        return f"Connetion to {address} on {port} failed: {err}"
     finally:
         s.close()
+
 
 def threader():
     while True:
         addr, port = q.get()
-        print(addr, port)
-        check_server(addr,port)
+        if "-w" in sys.argv:
+            with open('./' + sys.argv[-1], 'a+') as f:
+                f.write(str(port) + ': "' + check_server(addr, port)+ '",\n')
+        check_server(addr, port)
         q.task_done()
+
 
 q = Queue()
 
 for i in range(50):
     t = threading.Thread(target=threader)
-    t.daemon=True
+    t.daemon = True
     t.start()
 
-for addr in iplist():
-    for port in range(101):
-        q.put((addr, port))
+if "ping" in sys.argv:
+    for addr in iplist():
+        for port in range(1,101):
+            q.put((addr, port))
+elif "-w" in sys.argv:
+    for port in range(1,101):
+        q.put((sys.argv[6], port))
 
 q.join()
