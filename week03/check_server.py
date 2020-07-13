a9dc06a0b87cda23ff4fcd16d213d94f6e1164ea
Allen <allentango@163.com>
2020-07-13 22:28:27 +0800

    Update check_server.py

--- week03/check_server.py
+++ week03/check_server.py
@@ -1,30 +1,65 @@
 import sys
+import time
+import socket
+import threading
+from queue import Queue
 
 '''
 pmap.py -n 4 -f ping -ip 192.168.0.1-192.168.0.100
 pmap.py -n 10 -f tcp -ip 192.168.0.1 -w result.json
 '''
-print(sys.argv)
 
-import socket
+# print("-" in sys.argv[6])
+
+def iplist():
+    if "-" in sys.argv[6]:
+        addrs = sys.argv[6].split("-")
+        addrs_list = []
+        start_iplist = addrs[0].split('.')
+        for i in range(int(addrs[0].split('.')[-1]), int(addrs[-1].split('.')[-1]) + 1):
+            start_iplist[-1] = str(i)
+            addrs_list.append('.'.join(start_iplist))
+        return addrs_list
+
 
 # socket.AF_INET /IPv4 socket; socket.AF_INET6 /IPv6 socket; socket.AF_UNIX /IPC(local socket)
 # _socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-HOST = "localhost"
-PORT = 5555
+# import os
+# HOST = "localhost"
+# os.system('ping ' + HOST)
+# PORT = 5555
+_lock = threading.Lock()
 
 def check_server(address, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # s.settimeout(40)
+    s.settimeout(0.5)
     print(f"Attempting to connect to {address} on {port} ")
     try:
         s.connect((address, port))
-        return f"Connected to {address} on {port}."
+        with _lock:
+            print(f"Connected to {address} on {port}.")
     except socket.error as err:
-        return f"Connetion to {address} on {port} failed: {err} "
+        print(f"Connetion to {address} on {port} failed: {err} ")
     finally:
         s.close()
 
-for port in range(50, 5501,50):
-    print(check_server(sys.argv[6],port))
+def threader():
+    while True:
+        addr, port = q.get()
+        print(addr, port)
+        check_server(addr,port)
+        q.task_done()
+
+q = Queue()
+
+for i in range(50):
+    t = threading.Thread(target=threader)
+    t.daemon=True
+    t.start()
+
+for addr in iplist():
+    for port in range(101):
+        q.put((addr, port))
+
+q.join()
